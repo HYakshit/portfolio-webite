91d2cacdd7875fd826b7b80823ec6591d80ca3bc
akshi <akshit628325@gmail.com>
2025-05-15 16:57:18 +0530

    skill sec updated

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    autoplay: true,
-    smartSpeed: 800,
-    loop:false,
-    dots: true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 800,
+  loop: false,
+  dots: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+const startYear = 2023;
+const currentYear = new Date().getFullYear();
+const yearRange =
+  startYear === currentYear ? `${currentYear}` : `${startYear}-${currentYear}`;
+document.getElementById("year-range").textContent = yearRange;
